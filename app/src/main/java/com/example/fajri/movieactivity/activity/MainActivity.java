bbecbb453ff9907075df9e658a360bcd428ca7ec
Fajri <fajri@Fajris-MacBook-Pro.local>
2018-01-20 19:28:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fajri/movieactivity/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.fajri.movieactivity.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.fajri.movieactivity.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
