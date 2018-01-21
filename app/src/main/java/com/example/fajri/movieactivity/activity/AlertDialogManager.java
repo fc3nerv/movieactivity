9085fefd7bfac419ebb5564ab88cd700ba24c59e
Fajri <fajri@Fajris-MacBook-Pro.local>
2018-01-21 08:43:48 +0700

    v1

---
+++ app/src/main/java/com/example/fajri/movieactivity/activity/AlertDialogManager.java
@@ -0,0 +1,29 @@
+package com.example.fajri.movieactivity.activity;
+
+/**
+ * Created by fajri on 1/20/18.
+ */
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.drawable.Drawable;
+
+import com.example.fajri.movieactivity.R;
+
+//import com.example.andre.project.R;
+
+public class AlertDialogManager {
+    public void showAlertDialog(Context context, String title, String message,
+                                Boolean status) {
+        AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+        alertDialog.setTitle(title);
+        alertDialog.setMessage(message);
+        if(status != null)
+            alertDialog.setIcon((status) ? r.mipmap.ic_action_bell : r.mipmap.ic_action_bell);
+        alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        alertDialog.show();
+    }
+}
